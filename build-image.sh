4da7c8e0aa7afb8ab1332f481b56da1c709c1e8d
supercobra <supercobra@metadot.com>
2021-07-05 18:07:31 -0500

    feat: added script to build image

---
+++ build-image.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "+=======================+"
+echo "| BUILDING DOCKER IMAGE |"
+echo "+=======================+"
+set -xe
+APP=/tmp/bamzooka
+rm -rf $APP # remove previous build if any
+git clone --depth=1 git@github.com:metadot/metadot-workspace.git $APP
+rm -f $APP/apps/bamzooka-backend/log/*
+rm -rf $APP/apps/bamzooka-backend/tmp
+
+cp -r $APP ./image/base
+ls -l ./image/base
+docker build  image/base -t supercobra/bamzooka
+set +xe
+echo "+=======================+"
+echo "|   IMAGE BUILD DONE    |"
+echo "+=======================+"
+
