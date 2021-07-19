8ce4e9d1693f46fbd595fcd2bf257f4683b82898
supercobra <supercobra@metadot.com>
2021-07-19 14:23:06 -0500

    feat: add docker image version # and ability to push to docker.com

--- build-image.sh
+++ build-image.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
-
+NAME="supercobra/bamzooka"
 echo "+=======================+"
 echo "| BUILDING DOCKER IMAGE |"
 echo "+=======================+"
+
+echo -n "Do you want to push to docker.com after the build? (y or n)"
+read wants_to_push
+
+if [ "$wants_to_push" != "${wants_to_push#[Yy]}" ] ;then
+    echo "OK, will push image to docker.com"
+    sleep 2
+else
+    echo "OK, just building then, no pushing anything..."
+    sleep 2
+fi
 set -xe
 APP=/tmp/bamzooka
 rm -rf $APP # remove previous build if any
@@ -12,11 +23,32 @@ rm -rf $APP/apps/bamzooka-backend/tmp
 
 cp -r $APP ./image/base
 ls -l ./image/base
-docker build  image/base -t supercobra/bamzooka
+VERSION=`date +%Y%m%d.%H%M%S`
+BASE_VERSION="2.0"
+FULLNAME=$NAME:$BASE_VERSION.$VERSION
+docker build  image/base -t $FULLNAME
 set +xe
 rm -rf $APP 
 
 echo "+=======================+"
 echo "|   IMAGE BUILD DONE    |"
 echo "+=======================+"
+
+if [ "$wants_to_push" != "${wants_to_push#[Yy]}" ] ;then
+echo
+echo "Pushing to docker.com..."
+docker image push $FULLNAME
+echo "+"
+echo "|"
+echo "|"
+echo "|"
+echo "| IMAGE pushed to docker.com |"
+echo "| VERSION: $FULLNAME"
+echo "|"
+echo "+----------> ALL DONE!!!"
+    sleep 2
+else
+    # nothing
+fi
+set -xe
 
