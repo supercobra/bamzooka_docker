f1fb8fd02942beb8fb64f26037d0ccf7b03eecec
supercobra <daniel@metadot.com>
2021-08-02 15:56:29 -0500

    WIP

--- build-bam-image.sh
+++ build-bam-image.sh
@@ -8,14 +8,13 @@ echo -n "Do you want to push to Docker after the build? (y/n)"
 read want_to_push
 if [ "$want_to_push" == "y" ]; then
   echo "OK, will push image to docker.com"
-    sleep 2
+  sleep 2
 else
-    echo "OK, just building then, no pushing anything..."
-    sleep 2
+  echo "OK, just building then, no pushing anything..."
+  sleep 2
 fi
 
 # Build the image
-
 
 set -xe
 APP=./image/base/bamzooka
@@ -24,7 +23,7 @@ git clone --depth=1 git@github.com:metadot/metadot-workspace.git $APP
 rm -f $APP/apps/bamzooka-backend/log/*
 rm -rf $APP/apps/bamzooka-backend/tmp
 
-VERSION=`date +%Y%m%d.%H%M%S`
+VERSION=$(date +%Y%m%d.%H%M%S)
 BASE_VERSION="2.0"
 FULLNAME=$NAME:$BASE_VERSION.$VERSION
 docker build image/base -t $FULLNAME
@@ -36,25 +35,36 @@ echo "|   IMAGE BUILD DONE    |"
 echo "+=======================+"
 
 if [ "$want_to_push" == "y" ]; then
-echo "Pushing image to docker.com"
-    sleep 2
-    set -xe
-    docker push $FULLNAME
-    # echo "Pushing latest as wel..."
-    # docker image tag $FULLNAME  $NAME:latest
-    # docker image push $NAME:latest
-    set +xe
-    echo "+"
-    echo "|"
-    echo "|"
-    echo "|"
-    echo "| IMAGE pushed to docker.com |"
-    echo "| VERSION: $FULLNAME"
-    echo "|"
-    echo "+----------> ALL DONE!!!"
+  echo "Pushing image to docker.com"
+  sleep 2
+  set -xe
+  docker push $FULLNAME
+  # echo "Pushing latest as wel..."
+  # docker image tag $FULLNAME  $NAME:latest
+  # docker image push $NAME:latest
+  update_launcher_script
+  set +xe
+  echo "+"
+  echo "|"
+  echo "|"
+  echo "|"
+  echo "| IMAGE pushed to docker.com |"
+  echo "| VERSION: $FULLNAME"
+  echo "|"
+  echo "+----------> ALL DONE!!!"
 else
-    echo "Not pushing image to docker.com"
-    sleep 2
+  echo "Not pushing image to docker.com"
+  sleep 2
 fi
 
+echo "REMEMBER to update the "
 exit 0
+
+update_launcher_script() {
+  # update git pull version line in launcher file
+  # sed replace a line that start with
+  sed -i 's:image="supercobra/bamzooka.*:image="$FULLNAME":g' ./launcher
+  git add launcher
+  git commit -m "Updated image version to $FULLNAME."
+  git push
+}
