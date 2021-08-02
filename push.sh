51c07ff94e4c093518f494e4568d3a74f7792910
supercobra <daniel@metadot.com>
2021-08-02 17:49:33 -0500

    WIP

--- push.sh
+++ push.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-FULLNAME="supercobra/bamzooka:2.0.20210730.121131"
-wants_to_push="y"
-if [ "$wants_to_push" != "[Yy]" ]
-then
-    echo
-    echo "Pushing to docker.com..."
-    docker image push $FULLNAME
-    echo "+"
-    echo "|"
-    echo "|"
-    echo "|"
-    echo "| IMAGE pushed to docker.com |"
-    echo "| VERSION: $FULLNAME"
-    echo "|"
-    echo "+----------> ALL DONE!!!"
-    sleep 2
-else
-    # nothing
+# check there is one argument input
+if [ $# -lt 1 ]
+  echo "$0 <image_name>"
+  exit 1
 fi
+
+
+FULLNAME=$1
+echo "Pushing to docker.com: $FULLNAME"
+docker image push $FULLNAME
+
+# update launcher script with new version
+sed -i 's:image="supercobra/bamzooka.*:image="$FULLNAME":g' ./launcher
+git add launcher
+git commit -m "Updated image version to $FULLNAME."
+git push
+echo "+"
+echo "|"
+echo "|"
+echo "|"
+echo "| IMAGE pushed to docker.com "
+echo "| Updated bamzooka_docker    "
+echo "| version and pushed.        "
+echo "| version and pushed.        "
+echo "| VERSION: $FULLNAME         "
+echo "|                            "
+echo "+----------> ALL DONE!!!"
+sleep 2
 set -xe
 
